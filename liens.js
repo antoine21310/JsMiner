2291ee0694679dd379b1635c3a01d5858ba8c863
Alexandre <37457003+Juvamine@users.noreply.github.com>
2018-03-16 22:39:25 +0100

    couocu

---
+++ liens.js
@@ -0,0 +1,11 @@
+$(function(){
+  
+   $('#Stats').click(function() {
+      document.getElementById("FrameStats").src="stats/index.html";
+   });
+
+   $('#Miner').click(function() {
+     document.getElementById("FrameStats").src="miner_monero.html";
+   });
+
+});
