5fefb9af6edf1d23fc2b41331aa34aaecf84fbff
Alexandre <37457003+Juvamine@users.noreply.github.com>
2018-03-18 16:40:49 +0100

    objectif
    réalisation de la barre d'objectif

--- js/stats.js
+++ js/stats.js
@@ -1,13 +1,23 @@
 setInterval(function() {
 
-  //affiche le monero et le dollars et l'euros dans la page stats
+
   $.post("php/coinhive.php", function(data){
     var monero = data.hashes/1000000*data.payout;
     var usd = monero * data.xmrToUsd;
+    var pourcentObjectif = monero / 0.02 * 100
+
+    //affichage de l'objectif
+    objectifActuel.style.width = pourcentObjectif + "%";
+    $("#pourcent").html(pourcentObjectif.toFixed(2) + "%");
+    pourcent.style.marginLeft = pourcentObjectif + "%";
+
+
+    //affiche le monero, le dollars et la puissance dans la page stats
     $("#monero").html("Total monero : "+monero.toFixed(8)+" XMR");
     $("#dollars").html("Total USD : "+usd.toFixed(5)+" $");
     $("#hashesPerSecond").html("Total H/s : "+data.hashesPerSecond.toFixed(5)+" H/s");
 
+    // affiche l'euros dans la page stats
     $.post("php/USDtoEUR.php", function(data){
       var euro = usd / parseFloat(data);
       $("#euro").html("Total euros : "+euro.toFixed(5)+" €");
@@ -18,8 +28,4 @@ setInterval(function() {
 
   }, "json");
 
-
-
-
-
-  },2000);
+},2000);
