ced7dafff6a721be313847d6dd6f742a0fba466f
Alexandre <37457003+Juvamine@users.noreply.github.com>
2018-03-18 01:16:25 +0100

    stat a jour

--- js/stats.js
+++ js/stats.js
@@ -1,11 +1,24 @@
 setInterval(function() {
 
-//affiche le monero, le dollars et l'euros dans la page stats
+  //affiche le monero et le dollars et l'euros dans la page stats
   $.post("php/coinhive.php", function(data){
     var monero = data.hashes/1000000*data.payout;
+    var usd = monero * data.xmrToUsd;
     $("#monero").html("Total monero : "+monero.toFixed(8)+" XMR");
-    $("#dollars").html("Total USD : "+data.xmrToUsd+" $");
+    $("#dollars").html("Total USD : "+usd.toFixed(5)+" $");
+
+    $.post("php/USDtoEUR.php", function(data){
+      var euro = usd / parseFloat(data);
+      $("#euro").html("Total euros : "+euro.toFixed(5)+" €");
+
+
+    }, "json");
+
 
   }, "json");
 
-},2000);
+
+
+
+
+  },2000);
--- php/USDtoEUR.php
+++ php/USDtoEUR.php
@@ -1,5 +1,27 @@
-$fichier = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml';
+<?php
 
-$xml = simplexml_load_file($fichier);
+$url = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml';
+$timeout = 10;
 
-print_r($xml);
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+
+if (preg_match('`^https://`i', $url))
+{
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+}
+
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Récupération du contenu retourné par la requête
+$page_content = curl_exec($ch);
+
+curl_close($ch);
+$balance = explode("'",$page_content);
+echo $balance[5];
+?> 
