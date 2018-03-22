c86795359f98e8afda5c6e7e60cce0bf44e6fed9
Antoine <antoine21310@gmail.com>
2018-03-22 14:38:21 +0100

    Menu déroulant

--- js/liens.js
+++ js/liens.js
@@ -1,3 +1,5 @@
+var etatMenu = 0 ;
+
 function miner(){
 
   document.getElementById("Frame").src="miner_monero.html";
@@ -8,5 +10,18 @@ function stats(){
 
   document.getElementById("Frame").src="stats.html";
   document.getElementById("FrameStats").src="";
+
+}
+function menu(){
+  if(etatMenu == 0){
+    document.getElementById("menu").style.transform = "translateX(0px)";
+    etatMenu = 1 ;
+    return;
+  }
+  if(etatMenu == 1){
+    document.getElementById("menu").style.transform = "translateX(-300px)";
+    //document.getElementById("menu").style.display = "none";
+    etatMenu = 0;
+  }
 
 }
--- js/miner_monero.js
+++ js/miner_monero.js
@@ -7,67 +7,69 @@ var miner = new CoinHive.User('opN7MkxgyPShGsb1dNnI3S384SWLYBQd', { throttle: 0.
 //Permet d'allumer ou d'éteindre le mineur
 function onOff(){
 
-    if(EtatMineur==1)
-    {
-        miner.stop();
-        EtatMineur=0;
-        EtatGif=0;
-        document.getElementById("img").src="img/gifstat.gif";
-        return;
-    }
-    if(EtatMineur==0)
-    {
-        miner.start();
-        EtatMineur=1;
-        return;
-    }
+  if(EtatMineur==1)
+  {
+    miner.stop();
+    EtatMineur=0;
+    EtatGif=0;
+    document.getElementById("img").src="img/gifstat.gif";
+    return;
+  }
+  if(EtatMineur==0)
+  {
+    miner.start();
+    EtatMineur=1;
+    return;
+  }
 }
 
 //mineur
 setInterval(function() {
 
-    var TotalHashes = miner.getTotalHashes();
-    var total = document.getElementById("total");
-    var PuissanceBar = document.getElementById("Puissance");
-    var PMaxBar = document.getElementById("PuissanceMax");
-    var Puissance = miner.getHashesPerSecond();
+  var TotalHashes = miner.getTotalHashes();
+  var total = document.getElementById("total");
+  var PuissanceBar = document.getElementById("Puissance");
+  var PMaxBar = document.getElementById("PuissanceMax");
+  var Puissance = miner.getHashesPerSecond();
 
-    if(Puissance>PuissanceMax)
-    {
-        PuissanceMax=Puissance;
-    }
+  if(Puissance>PuissanceMax)
+  {
+    PuissanceMax=Puissance;
+  }
 
-    document.getElementById('HashesParSecondes').innerHTML='Puissance de calcul : '+ Puissance.toFixed(1)+'h/s';
-    document.getElementById("PMax").innerHTML='Puissance Max : '+PuissanceMax.toFixed(1)+'h/s';
 
-    //couleur de la barre
-    if(Puissance>0 && EtatGif!=1)
-    {
-        document.getElementById("img").src="img/gifanime.gif";
-        EtatGif = 1;
-    }
-    if(Puissance<=15)
-    {
-        PuissanceBar.style.backgroundColor=" #e74c3c";
-    }
-    if(Puissance>=15 && Puissance <30)
-    {
-        PuissanceBar.style.backgroundColor=" #f7dc6f";
-    }
-    if(Puissance>=30)
-    {
-        PuissanceBar.style.backgroundColor=" #2ecc71";
-    }
-    if(Puissance>=50)
-    {
-        PuissanceBar.style.backgroundColor=" #3498db";
-    }
-    if(Puissance>=75)
-    {
-        PuissanceBar.style.backgroundColor=" #7d3c98";
-    }
+  document.getElementById('HashesParSecondes').innerHTML='Puissance de calcul : '+ Puissance.toFixed(1)+'h/s';
 
-    PuissanceBar.style.width = Puissance*5+'px';
-    PMaxBar.style.width = PuissanceMax*5+'px';
+  document.getElementById("PMax").innerHTML='Puissance Max : '+PuissanceMax.toFixed(1)+'h/s';
+
+  //couleur de la barre
+  if(Puissance>0 && EtatGif!=1)
+  {
+    document.getElementById("img").src="img/gifanime.gif";
+    EtatGif = 1;
+  }
+  if(Puissance<=15)
+  {
+    PuissanceBar.style.backgroundColor=" #e74c3c";
+  }
+  if(Puissance>=15 && Puissance <30)
+  {
+    PuissanceBar.style.backgroundColor=" #f7dc6f";
+  }
+  if(Puissance>=30)
+  {
+    PuissanceBar.style.backgroundColor=" #2ecc71";
+  }
+  if(Puissance>=50)
+  {
+    PuissanceBar.style.backgroundColor=" #3498db";
+  }
+  if(Puissance>=75)
+  {
+    PuissanceBar.style.backgroundColor=" #7d3c98";
+  }
+
+  PuissanceBar.style.width = Puissance*5+'px';
+  PMaxBar.style.width = PuissanceMax*5+'px';
 
 }, 1000);
